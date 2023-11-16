aeb4a961cf07ad70e79833a576d820887d5e117d
Dhriti <dhriti.baruah20@gmail.com>
2023-11-16 19:37:55 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $characterName = "John";
+        $characterAge = 35;
+        echo "<h1>Hello World! My name is $characterName. I am $characterAge</h1>";
+        echo "<hr>";
+        $characterName = "Mike";
+        echo "<p>This is the first php page www.$characterName at the age of $characterAge</p>"
+    ?>
+</body>
+</html>
