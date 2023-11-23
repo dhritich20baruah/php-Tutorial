64d931fbd0d440a7af4c55a6402e5a784218c1e0
Dhriti <dhriti.baruah20@gmail.com>
2023-11-23 23:38:13 +0530

    calculator

--- index.php
+++ index.php
@@ -6,66 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        $characterName = "John";
-        $characterAge = 35;
-        echo "<h1>Hello World! My name is $characterName. I am $characterAge</h1>";
-        echo "<hr>";
-        $characterName = "Mike";
-        echo "<p>This is the first php page www.$characterName at the age of $characterAge</p>";
-        $phrase = "Hello World! My name is Dhriti";
-        $phrase[0] = "Y"; #replacing h with y
-        echo strtoupper($phrase);
-        echo "<hr>";
-        echo strtolower($phrase);
-        echo "<hr>";
-        echo strlen($phrase);
-        echo "<hr>";
-        echo $phrase[2];
-        echo "<hr>";
-        echo str_replace("Yello", "Hi", $phrase); #replaces words
-        echo "<hr>";
-        echo  substr($phrase, 6, 5);
-        echo "<h2>Working with numbers</h2>";
-        echo 5.7 * 9;
-        echo "<hr>";
-        echo 10 % 3; # modulus operator returns the remainder
-        echo "<hr>";
-        $num = 10;
-        $num++;
-        echo $num;
-        echo "<hr>";
-        $num--;
-        echo $num;
-        echo "<hr>";
-        $num *= 25;
-        echo $num;
-        echo "<hr>";
-        echo abs(-100); #absolute value
-        echo "<hr>";
-        echo pow(2,4);
-        echo "<hr>";
-        echo sqrt(196);
-        echo "<hr>";
-        echo max(2, 10);
-        echo min(2, 10);
-        echo round(3.2);
-        echo ceil(3.3);
-        echo floor(3.3); 
-        echo "<hr>";
-        echo "<h1>Getting user input</h1>";
-        echo "<br>";
-    
-    
-
-    ?>
-        <form action="php-server.php" method="get">
-            Name: <input type="text" name="name" id="name">
+        <form action="index.php" method="get">
+            First number: <input type="number" name="num1" id="num1">
+            <br>
+            Second number: <input type="number" name="num2" id="num2">
             <br>
             <button type="submit">SUBMIT</button>
         </form>
 
         <br>
-        <?php echo $_GET["name"] ?>
+        <p>Sum :<?php echo $_GET["num1"] + $_GET["num2"]?></p>
 </body>
 </html>
